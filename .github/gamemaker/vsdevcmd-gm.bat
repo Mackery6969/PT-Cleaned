ecd8705bd93dd9ac3ba78a1fe41a5fede5d13b86
Mackery <davishayden32@gmail.com>
2026-07-06 20:02:49 -0500

    leftover from git actions

--- .github/gamemaker/vsdevcmd-gm.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set "PATH=C:\Windows\System32;C:\Windows;C:\Windows\System32\Wbem;C:\Program Files (x86)\Microsoft Visual Studio\Installer;%PATH%"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" %*
